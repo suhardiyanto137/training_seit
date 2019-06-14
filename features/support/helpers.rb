c6d63b335f0ce790ba9c7fcdbe31eed094cdb71d
suhardiyanto137 <suhardiyanto137@gmail.com>
2019-06-14 13:41:33 +0700

    Souce test suha

--- features/support/helpers.rb
+++ features/support/helpers.rb
@@ -2,12 +2,16 @@ require_relative 'helpers/global_helper'
 require_relative 'helpers/automation_helper'
 require_relative 'helpers/wait_for_ajax_helper'
 require_relative 'helpers/checkout_helper'
+require_relative 'helpers/tax_library_helper'
+require_relative 'helpers/create_tax_singleoutlet'
 
 module Helper
   include AutomationHelper
   include CustomWorld
   include WaitForAjax
   include CheckoutHelper
+  include TaxLibrary
+  include CreateTaxSingleOutlet
 end
 
 World(Helper)
